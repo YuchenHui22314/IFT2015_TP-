b0ec07dc6e3d243405d09e77ccc098c4993c388c
YuchenHui22314 <yuchen22314@gmail.com>
2021-11-30 12:54:45 -0500

    input-output complete

--- MuzStream.java
+++ MuzStream.java
@@ -1,14 +1,56 @@
 /**
  * MuzStream requests playlistCapacity playlistLimit numberofFillings K
  */
+import ca.umontreal.adt.list.FavoritesListMTF;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class MuzStream {
 
     public static void main(String[] args) {
+        //analyse arguments
         String fileName = args[0];
         int playlistCapacity = Integer.parseInt(args[1]);
         int playlistLimit = Integer.parseInt(args[2]);
         int numberofFillings = Integer.parseInt(args[3]);
         int topK = Integer.parseInt(args[4]);
+
+        //total time passed since the application is launched
+        int timePassed;
+        
+        //list for stocking all the songs in the request file
+        ArrayList<Song> songList = new ArrayList<>();
+        
+        //create a playlist.
+        Playlist playlist = new Playlist();
+
+        //crate a favoriteList for TOP-K song(s)
+        FavoritesListMTF<Song> topKList = new FavoritesListMTF<>();
+        
+
+        //fill the songlist by reading all the songs in the reauest file.
+        //code inspired by SimpleFastReader.java in the package
+        //ca.umontreal.maps
+        
+        try {
+	    File input = new File( fileName );
+	    Scanner reader = new Scanner( input );
+	    // read the sequence
+        String line;
+	    while( reader.hasNextLine() ) {
+		line = reader.nextLine();
+        String[] infoPieces = line.split("\\t");
+        System.out.println(infoPieces[0]+infoPieces[1]+infoPieces[2]);
+        songList.add(new Song(infoPieces));
+	    }
+        } catch( FileNotFoundException e ) {
+            System.out.println( "Something's wrong, file not found!" );
+            e.printStackTrace();
+	    }
+
+        System.out.println(songList);
 
     }
 }
--- Song.java
+++ Song.java
@@ -1,8 +1,17 @@
-public class Song {
+import javax.xml.namespace.QName;
+
+public class Song implements Comparable<Song>{
     private String artist;
     private String songName;
     private int time;
+    
+    public Song(String[] infos){
+       if (infos.length != 3) throw new IllegalArgumentException("not a song"); 
+       this.artist = infos[0];
+       this.songName = infos[1];
+       this.time = Integer.parseInt(infos[2]);
 
+    }
     public String getArtist() {
         return artist;
     }
@@ -33,6 +42,20 @@ public class Song {
         return (this.artist.equals(s.getArtist()) 
         && this.songName.equals(s.getSongName()) 
         && (this.time == s.getTime()));
+    }
+    
+    //favoriteList require that song should be comparable,
+    //although it is useless to be comparable.
+    //So we just implement a useless method to cope with it.
+    @Override
+    public int compareTo(Song o) {
+        return 0;
+    }
+
+    @Override
+    public String toString(){
+        return ("[" + this.artist + ", " 
+        + this.songName + ", " + this.time + "]");
     }
 
     
